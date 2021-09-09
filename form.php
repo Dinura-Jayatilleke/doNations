bf8ddd44d80c417e3cdcce0103fb7f4299601ebb
ThisumiSamarasekara <77894368+ThisumiSamarasekara@users.noreply.github.com>
2021-09-09 19:29:21 +0530

    isset php

--- form.php
+++ form.php
@@ -5,3 +5,10 @@
 	$dbname= "commentsystem";
 	
 	$con= mysqli_connect($server, $username, $password, $dbname);
+	
+	if(isset($_POST['submit'])){
+
+		if(!empty($_POST['uname']) && !empty($_POST['addcomment'])){
+
+			$name = $_POST['uname'];
+			$comment = $_POST['addcomment'];
