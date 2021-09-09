c7d698a37e9120996b56ed5e351c010b8874e527
Dinura-Jayatilleke <dinura143@gmail.com>
2021-09-09 21:06:21 +0530

    adding the php actions
    those files were locally created earlier, and then added to the main file

--- Products/Product.php
+++ Products/Product.php
@@ -2,6 +2,46 @@
   session_start();
   $db_name = "donations";
   $connection = mysqli_connect("localhost","root","",$db_name);
+
+  if(isset($_POST["add"])){
+    if(isset($_SESSION["shopping_cart"])){
+      $item_array_id = array_column($_SESSION["shopping_cart"],"product_id");
+        if(!in_array($_GET["id"],$item_array_id)){
+          $count = count($_SESSION["shopping_cart"]);
+          $item_array = array(
+           'product_id' => $_GET["id"],
+           'product_name' => $_POST["hidden_name"],
+           'product_price' => $_POST["hidden_price"],
+           'product_quantity' => $_POST["quantity"],
+            );
+            $_SESSION["shopping_cart"][$count] = $item_array;
+            echo '<script>window.location="cart.php"</script>';
+            }else{
+            echo '<script>alert("Product is already in  the cart")</script>';
+            echo '<script>window.location="cart.php"</script>';
+          }
+        }else{
+            $item_array = array(
+                'product_id' => $_GET["id"],
+                'product_name' => $_POST["hidden_name"],
+                'product_price' => $_POST["hidden_price"],
+                'product_quantity' => $_POST["quantity"],
+            );
+            $_SESSION["shopping_cart"][0] = $item_array;
+        }
+    }
+
+    if(isset($_GET["action"])){
+        if($_GET["action"] == "delete"){
+            foreach($_SESSION["shopping_cart"] as $keys => $value){
+                if($value["product_id"] == $_GET["id"]){
+                    unset($_SESSION["shopping_cart"][$keys]);
+                    echo '<script>alert("Product has been removed")</script>';
+                    echo '<script>window.location="cart.php"</script>';
+                }
+            }
+        }
+    }
 ?>
   
 <!DOCTYPE html>
