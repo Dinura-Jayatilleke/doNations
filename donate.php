3cda8856131c4a1a1b40fc7644a2dea0b7ac1a24
Gihan Karunarathna <gihanrandula1@gmail.com>
2021-09-11 10:01:02 +0530

    donate .php
    donation  php codes

--- donate.php
+++ donate.php
@@ -1,1 +1,34 @@
+<?php
 
+include_once 'donate.php';
+
+$conn =new mysqli("localhost","root","","donate");
+if(!$conn)
+{
+	die("connection failed".mysqli_connect_error());
+}
+else
+{
+	$fullName =$_POST["FullName"];
+	$email =$_POST["Email"];
+    $phonenumber =$_POST["phonenumber"];
+	$phonenumber =$_POST["Amount"];
+
+
+$sql="INSERT INTO donate(FullName,Email,phonenumber,Amount)
+    VALUES ('$fullName','$email','$phonenumber','$phonenumber')";
+
+if(mysqli_query($conn,$sql))
+{
+	echo "Donation Successfull..";
+	
+}
+else
+{
+	echo "Donation Successfull..";
+}
+
+}
+
+
+?>
