ea03e0b4fd12a64e000fffa6766c2534273e1a9e
Dinura-Jayatilleke <dinura143@gmail.com>
2021-09-16 14:40:04 +0530

    Update Product.php

--- Product.php
+++ Product.php
@@ -58,7 +58,7 @@ if (isset($_POST['add'])){
     <!-- Bootstrap CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="Products/Productstyles.css">
+    <link rel="stylesheet" href="Products/ProductStyles.css">
 </head>
 <body>
 
