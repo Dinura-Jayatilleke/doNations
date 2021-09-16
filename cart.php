71ee67882b3bdccbf40a76b7b388bf78d2862e8d
Dinura-Jayatilleke <dinura143@gmail.com>
2021-09-16 15:46:51 +0530

    Update cart.php

--- cart.php
+++ cart.php
@@ -2,18 +2,14 @@
 
 session_start();
 
-require_once ("Products/php/CreateDb.php");
+require_once ("Products/php/connection.php");
 require_once ("Products/php/component.php");
-
-$db = new CreateDb("Productdb", "Producttb");
 
 if (isset($_POST['remove'])){
   if ($_GET['action'] == 'remove'){
       foreach ($_SESSION['cart'] as $key => $value){
           if($value["product_id"] == $_GET['id']){
               unset($_SESSION['cart'][$key]);
-              /*echo "<script>alert('Product has been Removed...!')</script>";
-              echo "<script>window.location = 'cart.php'</script>";*/
           }
       }
   }
